c444dbe509048384c7bfbd8d2fe03e3998bc9ced
Roman <kovajkin2012@yandex.ru>
2023-06-15 15:04:23 +0300

    Initial Commit

---
+++ AutoDockTestTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AutoDockTestTask
+//
+//  Created by Роман Ковайкин on 15.06.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
